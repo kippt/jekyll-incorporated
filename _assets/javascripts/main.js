39a1ceee68ea4d4fc0a471d502dc1c46933f6ceb
Karri Saarinen <karri@karrisaarinen.com>
2013-11-26 13:53:46 -0800

    no sticky header

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,15 +1,3 @@
-$(window).scroll(function() {
-    var scrollBottom = $(window).scrollTop() + $(window).height();
-    
-    if ($(window).scrollTop() > 100) {
-        $('body').addClass('nav-hidden');
-    }
-
-    else {
-        $('body').removeClass('nav-hidden');
-    }
-});
-
 $(".full img").on("click", function() {
   $(this).toggleClass("zoom");
 });
