eae43ed3ca7121949f39b4698f5b1b5013cee27e
Zach Supalla <zsupalla@gmail.com>
2014-09-30 22:01:41 -0700

    More verbose travis script

--- travis.sh
+++ travis.sh
@@ -3,9 +3,11 @@
 if [[ $TRAVIS_BRANCH == 'staging' ]]
 then
   bundle exec rake site:staging
+  echo "Deploying to staging: http://spark.github.io/staging-blog"
 elif [[ $TRAVIS_BRANCH == 'master' ]]
 then
   bundle exec rake site:publish
+  echo "Deploying to blog.spark.io"
 else
   echo "Nothing to do here!"
 fi
