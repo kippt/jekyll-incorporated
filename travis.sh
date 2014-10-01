9565f6c4483c840fa596695d089124e829b0f987
Zach Supalla <zsupalla@gmail.com>
2014-09-30 21:31:47 -0700

    Fixed travis.sh

--- travis.sh
+++ travis.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [[ $TRAVIS_BRANCH == 'staging' ]]
+then
   bundle exec rake site:staging
 elif [[ $TRAVIS_BRANCH == 'master' ]]
+then
   bundle exec rake site:publish
+else
+  echo "Nothing to do here."
 fi
